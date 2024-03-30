1aa676a6094592fb8f15d1754ca347ed188e5e4f
Alecs Joita <horilarila@gmail.com>
2024-03-30 15:45:55 +0100

    front page made

--- www/index.php
+++ www/index.php
@@ -1,1 +1,25 @@
 
+<?php include 'header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receptenboek</title>
+    <style>
+    </style>
+</head>
+
+<body>
+    <header>
+        <img src="images/Tiroler-groestl-a-potato-dish-iStock-683770218.jpg" alt="">
+    </header>
+    <br>
+    <a href="recepten.php">Receptenboek</a>
+
+    <br><br>
+</body>
+
+</html>
